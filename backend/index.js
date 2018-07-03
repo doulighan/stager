cd58d99f59f1b61421a7651593cc6860bfd1a63b
doulighan <doulighan@gmail.com>
2018-07-03 10:02:24 -0400

    moving resources into backend

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,7 @@ var modifier = '';
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(expressForm({ 
-  uploadDir: '../tmp',
+  uploadDir: './tmp',
   multiples: true, 
   keepExtensions: true,
   keepExtensions: true,
@@ -47,7 +47,7 @@ app.get('/upload', function(req, res) {
 
 app.post('/upload', function (req, res) {
   req.files.filetoupload.forEach(function (file) {
-    var newpath = '../outputs/' + projectName + '/' + subFolder + '/' + modify(modifier) + 'images/' + file.name;
+    var newpath = './outputs/' + projectName + '/' + subFolder + '/' + modify(modifier) + 'images/' + file.name;
     console.log(newpath, file.path);
     fs.rename(file.path, newpath);
     generateHTML(file.name);
@@ -85,7 +85,7 @@ function modify() {
 
 function createFileStructure(projectName, subFolder, modifier) {
   console.log('Project Name: ' + projectName, 'Sub Folder: ' + subFolder); 
-  mkdirp('../outputs/' + projectName + '/' + subFolder + '/' + modify() + 'images/', function(err) {
+  mkdirp('./outputs/' + projectName + '/' + subFolder + '/' + modify() + 'images/', function(err) {
     if(err) console.error(err);
     else console.log('pow!');
   })
@@ -93,14 +93,14 @@ function createFileStructure(projectName, subFolder, modifier) {
 
 function generateHTML(name) {
   const NAME = name.substring(0, name.lastIndexOf('.'));
-  const path = '../outputs/' + projectName + '/' + subFolder + '/' + modify() + 'images/' + name;
+  const path = './outputs/' + projectName + '/' + subFolder + '/' + modify() + 'images/' + name;
   console.log(path);
   console.log(projectName);
   const htmlWRITE = NAME + '.html';
   const HTML = 
   '<!doctype html><html><head><meta charset="utf-8"><meta http-equiv="X-UA-Compatible" content="IE=edge"><meta name="viewport" content="width=device-width, initial-scale=1"><meta name="description" content=""><meta name="author" content=""><title>' + NAME + '</title></head><body class="overflow-hidden" style="background:#000;"><img width="100%" src="./' + 'images/' + name + '"></body></html>';
 
-  fs.writeFile("../outputs/" + projectName + "/" + subFolder + "/" + modify() + htmlWRITE, HTML, function(err) {
+  fs.writeFile("./outputs/" + projectName + "/" + subFolder + "/" + modify() + htmlWRITE, HTML, function(err) {
     if(err) {
       return console.log(err);
     }
@@ -111,7 +111,7 @@ function generateHTML(name) {
 function writePathToFile(name) {
   const NAME = name.substring(0, name.lastIndexOf('.'));
   var text = 'http://sites.spiderboost.com/' + projectName + '/' + subFolder + '/' + modify(modifier) + NAME + '.html' + '\n\n';
-  fs.appendFile("../outputs/" + projectName + '/' + 'paths.txt', text, function(err) {
+  fs.appendFile("./outputs/" + projectName + '/' + 'paths.txt', text, function(err) {
     if(err) {
       return console.log(err);
     }
